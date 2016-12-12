c5a6ad282db76f9c46549650dbf0ff39fa4cb815
Sebastian <Sebastian>
2016-12-12 13:35:50 -0500

    New table with authors

--- Forest.r
+++ Forest.r
@@ -28,10 +28,22 @@ class(mydata)
 
 author <- mydata[, c(2,7)]
 
-author_1 <- data.frame(ID = "", author = "")
+## For to authors ###
+author_1 <- data.frame(ID = character(), author = character(), stringsAsFactors = FALSE)
+list_ids <- unique(author$ID)
+
+for (i in list_ids) {
+  row_1 =  author[author$ID == i,]
+  newrow = data.frame(ID= i, strsplit(as.character(row_1$author), "and"))
+  colnames(newrow) = c("ID", "author")
+  author_1 = rbind(author_1, newrow)
+}
 
 
+#### Others ####
 dbWriteTable(conn = db, name = "article", value = article)
 
 dbListTables(db)
 dbReadTable(db, "article")
+
+
