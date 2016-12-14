ef895a710efd97274c2f91889bef4c0dfde95a15
Sebastian <Sebastian>
2016-12-14 11:20:17 -0500

    Authors ok but with whitespace

--- Forest.r
+++ Forest.r
@@ -17,8 +17,6 @@ mydata$X <- NULL
 
 article <- mydata[,c(2, 1, 3,36, 43, 41  )]
 
-
-
 #### Separate authors from mydata ####
 
 author <- mydata[, c(2,7)]
@@ -29,7 +27,7 @@ list_ids <- unique(author$ID)
 
 for (i in list_ids) {
   row_1 =  author[author$ID == i,]
-  newrow = data.frame(ID= i, strsplit(as.character(row_1$author), "and "))
+  newrow = data.frame(ID= i, strsplit(as.character(row_1$author), "and"))
   colnames(newrow) = c("ID", "author")
   author_1 = rbind(author_1, newrow)
 }
@@ -39,7 +37,7 @@ for (i in list_ids) {
 edgelist <- data.frame(Source = character(), Target = character(), stringsAsFactors = FALSE)
 table_ids <- table(author_1$ID)
 table_ids_0 <- data.frame(table_ids)
-table_ids_1 <- table_ids[table_ids_0$Freq > 1,]
+table_ids_1 <- table_ids_0[table_ids_0$Freq > 1,]
 list_ids_1 <- unique(table_ids_1$Var1)
 
 for (i in list_ids_1) {
